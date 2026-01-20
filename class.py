5b42f03c7a5a7a1c76ff146fd18d14f6a7398674
arpita1403 <arpitaprasad1403@gmail.com>
2026-01-20 12:57:21 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,14 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        self.parent=None
+root=None
+a= [1,10,11,8,6,12,14,7]
+print(root)
+root=None(a[0])
+print(root.data)
+b=Node(a[1])
+print(b)
+print(b.data)
