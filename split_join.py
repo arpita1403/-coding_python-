981f3eb8a802adf36e0485944a6b1c82964aaa1f
arpita1403 <arpitaprasad1403@gmail.com>
2026-04-07 12:02:49 +0530

    split_join.py

---
+++ split_join.py
@@ -0,0 +1,13 @@
+a= "my name is arpita"
+b=a.split()
+print(b)
+c=" ".join(b)
+print(c)
+
+
+
+sentence="i am arpita"
+words=sentence.split()
+words.reverse()
+reversed_sentence=" ".join(words)
+print(reversed_sentence)
